499030e610e1df5a7928ff847ec507c8cebace2b
Marc Delalonde <marcdelalonde@gmail.com>
2021-11-09 18:22:16 +0100

    Add some request query parameters for parsing

--- src/middlewares/guard.js
+++ src/middlewares/guard.js
@@ -5,5 +5,11 @@ module.exports.parseQuery = (req, res, next) => {
   if (req.headers['am-ref-fields'] && typeof req.headers['am-ref-fields'] === 'string') {
     req.headers['am-ref-fields'] = JSON.parse(req.headers['am-ref-fields']);
   }
+  if (req.query.filters && typeof req.query.filters === 'string') {
+    req.query.filters = JSON.parse(req.query.filters);
+  }
+  if (req.query.segment) {
+    req.query.segment = JSON.parse(req.query.segment);
+  }
   next();
 };
